c1ab407f376e0bd53945870f752b560eefa1fa2d
Nethmin <dulsaranethmin@gmail.com>
2021-03-06 13:55:41 +0530

    login-page-created

--- client/src/Component/FormLogin.js
+++ client/src/Component/FormLogin.js
@@ -4,34 +4,47 @@ import {Link} from 'react-router-dom';
 import {useState} from 'react';
 
 const FormLogin =() => {
+    
+    //set an obeject to store user input data  
+    const [logindata,setLogindata]=useState({
+        loginAddress:'',
+        loginPassword:''
+    })
 
-    const [logindata,setLogindata]=useState('');
-
-    let handleLogin=(e)=>
+    const handleLogin =(e)=>
     {
-        setLogindata(e.target.value);
-        console.log(e.target);
-    }  
+        // e.target is an object.
+        // name and value are attributes of that object
+       const {name,value}=e.target;
+       console.log(name+':'+value);
+       setLogindata(prevLogindata=>
+        {
+            //console.log(...prevLogindata)
+            return {...prevLogindata,[name]:value};
+        })
+    }
 
+    const handleClick =(e)=>
+    {
+        e.preventDefault();
+        console.log(logindata);
+    }
 
 
     return (  
         <div>
         <form>
             <h2>Login</h2>
-            <input type='text' name='login-address'  autoComplete='off' placeholder='email or username' onChange={(e)=>handleLogin(e)} />
-            <input type='password' placeholder='Password' name='login-password' onChange={(e)=>setLogindata(e.target.value)}/>
+            <input type='text' name='loginAddress'  autoComplete='off' placeholder='email or username' onChange={(e)=>handleLogin(e)} />
+            <input type='password' placeholder='Password' name='loginPassword'onChange={(e)=>handleLogin(e)} />
             <div>            
-            <Button text='Login' onClick={handleLogin}/>
+            <Button text='Login' onClick={handleClick}/>
             <span><Link to='/register'>Register</Link></span>
            </div>
-           {loindata}
+           
         </form>
-        </div>
-
-     
+        
+        </div>  
    );
 }
- 
-
 export default FormLogin
