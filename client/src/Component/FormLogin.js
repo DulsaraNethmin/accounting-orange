f140cbfb739cde5bde8ac03cc72749713c250fe8
Nethmin <dulsaranethmin@gmail.com>
2021-03-06 07:46:19 +0530

    start-orange-server

--- client/src/Component/FormLogin.js
+++ client/src/Component/FormLogin.js
@@ -1,19 +1,31 @@
-import Button from "./Button"
-import './formlogin.css'
+import Button from "./Button";
+import './formlogin.css';
 import {Link} from 'react-router-dom';
-
+import {useState} from 'react';
 
 const FormLogin =() => {
+
+    const [logindata,setLogindata]=useState('');
+
+    let handleLogin=(e)=>
+    {
+        setLogindata(e.target.value);
+        console.log(e.target);
+    }  
+
+
+
     return (  
         <div>
         <form>
             <h2>Login</h2>
-            <input type='text' placeholder='email or username'/>
-            <input type='password' placeholder='Password'/>
+            <input type='text' name='login-address'  autoComplete='off' placeholder='email or username' onChange={(e)=>handleLogin(e)} />
+            <input type='password' placeholder='Password' name='login-password' onChange={(e)=>setLogindata(e.target.value)}/>
             <div>            
-            <Button text='Login'/>
+            <Button text='Login' onClick={handleLogin}/>
             <span><Link to='/register'>Register</Link></span>
-            </div>
+           </div>
+           {loindata}
         </form>
         </div>
 
